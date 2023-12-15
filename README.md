99b4b92e3f557936a4ea5ac1d76ab9aa401a20d9
Jack <39641663+luarss@users.noreply.github.com>
2023-12-15 13:46:13 +0000

    add instructions for running and hyperparameters argparse

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 Objective is to build a LLM Chatbot that is able to answer user queries for OpenROAD.
 
+## Quickstart
+
+```
+# select chunking strategy
+python chunk.py --chunk-size 200 --overlap 20
+
+# regenerate embeddings (if loading old embedding)
+python chat.py --gen-embeddings --max-tokens 200 --temp 0.2 --n-ctx 8000
+```
+
 ## TODOs
 - Test code-focused LLM vs instruct-focused LLM
 - Gather RAG dataset
--- chat.py
+++ chat.py
@@ -1,9 +1,22 @@
 from llama_cpp import Llama
 from langchain.prompts import ChatPromptTemplate
-from embed import load_existing_embeddings
+import argparse
+from utils import load_docs_from_jsonl
+from embed import load_existing_embeddings, generate_new_embeddings
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--gen-embeddings", action="store_true", help="Regenerate embeddings")
+parser.add_argument("--max-tokens", type=int, help="max_tokens hyperparameter", default = 200)
+parser.add_argument("--temp", type=float, help="temperature hyperparameter", default = 0.2)
+parser.add_argument("--n-ctx", type=int, help="n_ctx hyperparameter", default = 8000)
+args = parser.parse_args()
+gen_embed = args.gen_embeddings
+max_tokens = args.max_tokens
+temp = args.temp
+n_ctx = args.n_ctx
 
 def create_completion(db, question,
-                        max_tokens=200, temperature=0.2):
+                        max_tokens, temperature, n_ctx):
     # Define the prompt template
     template = PROMPT_TEMPLATE
     prompt = ChatPromptTemplate.from_template(template)
@@ -14,7 +27,7 @@ def create_completion(db, question,
     print(final_prompt)
     
     # Step 7: Use llama-cpp-python as a prototype. 
-    llm = Llama(model_path="../llama.cpp/models/mistral-instruct/ggml-model-q4_0.gguf", n_ctx=8000)
+    llm = Llama(model_path="../llama.cpp/models/mistral-instruct/ggml-model-q4_0.gguf", n_ctx = n_ctx)
     output = llm.create_completion(final_prompt,
                                    suffix=None,
                                    max_tokens=max_tokens, # set this to 0 for no limit on tokens (depend on n_ctx)
@@ -44,10 +57,13 @@ PRE_PROMPT_QUERY = "Pay attention and remember the information below, which will
 PROMPT_QUERY = "\nAccording to only the information in the document sources provided within the context above, "
 
 PROMPT_TEMPLATE = """%s%s{context}%s%s{question}"""%(TRIPLE_QUOTES, PRE_PROMPT_QUERY, TRIPLE_QUOTES, PROMPT_QUERY)
-
-db, retriever = load_existing_embeddings()
+if gen_embed: 
+    chunks = load_docs_from_jsonl("./tempdata/data.jsonl")
+    db, retriever = generate_new_embeddings(chunks)
+else:
+    db, retriever = load_existing_embeddings()
 question = "What function does the OpenROAD module name odb serve?"
-context, output = create_completion(db, question)
+context, output = create_completion(db, question, max_tokens, temp, n_ctx)
 print(context)
 print(output["choices"][0]["text"])
 
